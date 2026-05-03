a6329d7f5aa5d3122e0e522ba0c816188f3193b7
Suvadeep Mukherjee <suvadeepm0709@gmail.com>
2026-05-03 21:59:48 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const myMap = new Map();
+        for(let i=0;i<s.length;i++){
+            const count = (myMap.get(s[i]) || 0) + 1;
+            myMap.set(s[i],count)
+        }
+
+        for(let j=0;j<t.length;j++){
+            const count = myMap.get(t[j]) -1 ;
+            myMap.set(t[j],count)
+        }
+
+        for(const [key,value] of myMap){
+            const isZero = value === 0;
+            if(!isZero) return false 
+        }
+        return true
+    }
+}
